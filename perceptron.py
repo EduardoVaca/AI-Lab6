3c3ebe2afd3edab76d1fb78aa51d4c4ac69e802c
EduardoVaca <eduardo.vaca.g@gmail.com>
2017-11-08 11:38:56 -0600

    Add perceptron class

--- perceptron.py
+++ perceptron.py
@@ -2,6 +2,22 @@
 AI Lab: Perceptron
 Author: Eduardo Vaca
 """
+import random
+
+class Perceptron(object):
+    """ Class that represents a Perceptron node from ANN
+    """
+
+    def __init__(self, n_inputs, lower_limit=None, upper_limit=None):
+        if not lower_limit:
+            lower_limit = 0.0001
+        if not upper_limit:
+            upper_limit = 0.01
+        self.threshold = random.uniform(lower_limit, upper_limit)
+        self.weights = [random.uniform(lower_limit, upper_limit) for _ in range(n_inputs)]
+
+    def __str__(self):
+        return 'Threshold: {}\nWeights: {}'.format(self.threshold, self.weights)
 
 
 def get_train_data(n_attributes, n_train):
@@ -39,6 +55,8 @@ def main():
     print(result)
     test = get_test_data(n_test)
     print(test)
+    p = Perceptron(n_attributes)
+    print(p)
 
 
 if __name__ == '__main__':
