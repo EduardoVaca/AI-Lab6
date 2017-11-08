a7849625fbfd684dfe578ec243c6f1828f3ea5fa
EduardoVaca <eduardo.vaca.g@gmail.com>
2017-11-08 14:22:52 -0600

    Perceptron train ready

--- perceptron.py
+++ perceptron.py
@@ -15,6 +15,24 @@ class Perceptron(object):
     def __str__(self):
         return 'Threshold: {}\nWeights: {}'.format(self.threshold, self.weights)
 
+    def train(self, x_train, y_train, learn_rate=0.005, iteration_limit=10):
+        error = 1.0
+        iteration = 0
+        while error > 0.0 and iteration < iteration_limit:
+            error = 0.0
+            for i_record in range(len(x_train)):
+                current_y = 1 if sum([x_train[i_record][i]*self.weights[i] for i in range(len(x_train[i_record]))]) >= self.threshold else 0
+                error += abs(y_train[i_record]-current_y)
+                self.re_weight(learn_rate, x_train[i_record], y_train[i_record], current_y)
+                print(self.weights)
+            iteration += 1
+        return False if error > 0.0 else True
+
+    def re_weight(self, learn_rate, x_data, y, y_obtained):
+        for i in range(len(x_data)):
+            weight_diff = learn_rate * (y-y_obtained) * x_data[i]
+            self.weights[i] += weight_diff
+
 
 def get_train_data(n_attributes, n_train):
     """Reads input and gets training data
@@ -28,8 +46,8 @@ def get_train_data(n_attributes, n_train):
     y_train = []
     for _ in range(n_train):
         current_record = input().replace(' ', '').split(',')
-        x_train.append([int(x) for x in current_record[:n_attributes]])
-        y_train.append(int(current_record[-1]))
+        x_train.append([float(x) for x in current_record[:n_attributes]])
+        y_train.append(float(current_record[-1]))
     return (x_train, y_train)
 
 def get_test_data(n_test):
@@ -39,7 +57,7 @@ def get_test_data(n_test):
     RETURNS:
     - list of test x data
     """
-    return [[int(x) for x in input().replace(' ', '').split(',')] for _ in range(n_test)]
+    return [[float(x) for x in input().replace(' ', '').split(',')] for _ in range(n_test)]
 
 def main():
     """Main program
@@ -47,12 +65,14 @@ def main():
     n_attributes = int(input())
     n_train = int(input())
     n_test = int(input())
-    result = get_train_data(n_attributes, n_train)
-    print(result)
+    data = get_train_data(n_attributes, n_train)
+    print(data)
     test = get_test_data(n_test)
     print(test)
     p = Perceptron(n_attributes)
     print(p)
+    r_train = p.train(data[0], data[1])
+    print(r_train)
 
 
 if __name__ == '__main__':
